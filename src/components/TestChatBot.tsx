c4db248e377524e740b3dd32bf51d49acb1dea0c
J00landa <jolandabrouwer@hotmail.com>
2025-06-29 14:09:05 +0200

    Updated TestChatBot.tsx

--- src/components/TestChatBot.tsx
+++ src/components/TestChatBot.tsx
@@ -828,7 +828,11 @@ Geef feedback in deze structuur:
 
     setIsLoading(true)
     try {
-      const payload: any = { 
+
+      }
+    }
+  }
+}      const payload: any = { 
         message, 
         useGrounding: aiModel === 'internet' ? useGrounding : false,
         aiModel 
